377f75b699363dca475b6f0f11c00cf83670323c
Aurélien GALTIER <aurelien.galtier@live.fr>
2017-12-29 22:09:49 +0100

    Add Scrapper

---
+++ Crawler.Scrapper/Scrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace Crawler.Scrapper
+{
+    public class Scrapper<TScrapper>
+    {
+        private readonly HtmlDocument document;
+
+        private readonly TScrapper instance;
+
+        public Scrapper(HtmlDocument document, TScrapper instance)
+        {
+            this.document = document;
+            this.instance = instance;
+        }
+
+        public Scrapper<TScrapper> Property<T>(Action<TScrapper, T> setter, Func<Scrapper<TScrapper>, T> scrap)
+        {
+            T value = scrap(this);
+            setter(this.instance, value);
+            return this;
+        }
+
+        public TScrapper Get()
+        {
+            return this.instance;
+        }
+
+        public IEnumerable<HtmlNode> Balise(string balise)
+        {
+            return this.document.DocumentNode.Descendants(balise);
+        }
+    }
+}
---
+++ Crawler.Scrapper/ScrapperExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace Crawler.Scrapper
+{
+    public static class ScrapperExtension
+    {
+        public static Scrapper<T> ScrapTo<T>(this HtmlDocument document)
+            where T : new()
+        {
+            return new Scrapper<T>(document, new T());
+        }
+
+        public static IEnumerable<HtmlNode> Balise(this IEnumerable<HtmlNode> nodes, string balise)
+        {
+            foreach (var htmlNode in nodes)
+            {
+                foreach (var descendant in htmlNode.Descendants(balise))
+                {
+                    yield return descendant;
+                }
+            }
+        }
+    }
+}
