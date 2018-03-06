dfa3b928230936ee7f759e13d773c8de738ef78c
Aurélien GALTIER <aurelien.galtier@live.fr>
2018-03-07 00:22:33 +0100

    Add Pattern logger on crawler

---
+++ src/Crawler.Pattern.Logging/PatternLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Pattern.Logging;
+
+namespace Crawler.Pattern.Logging
+{
+    public class PatternLogger : ICrawlerLogger
+    {
+        private ILogger logger;
+
+        public PatternLogger(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Error(Exception any)
+        {
+            this.logger.Error(any, "Error on crawler");
+        }
+    }
+}
---
+++ src/Crawler.Pattern.Logging/TraceWriterBindingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Pattern.Core.Interfaces;
+using Pattern.Core.Interfaces.Factories;
+using Pattern.Logging;
+
+namespace Crawler.Pattern.Logging
+{
+    public static class TraceWriterBindingExtensions
+    {
+        public static IKernel BindCrawlerLogger(this IKernel kernel)
+        {
+            kernel.Bind(
+                typeof(ILogger),
+                new TypeFactory(typeof(PatternLogger), kernel));
+
+            return kernel;
+        }
+    }
+}
